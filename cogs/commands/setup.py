b11dfe08894bc7d80238fab927e5db58127e6ce0
ismaeelakram <mathcenter562@gmail.com>
2020-06-21 21:58:42 -0400

    Add setup!

---
+++ cogs/commands/setup.py
@@ -0,0 +1,62 @@
+import discord
+from discord.ext import commands
+from discord.utils import get
+from utils import embed, config
+import json
+
+
+class CreateVerify(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.vars = json.load(open("./vars.json"))
+        self.emojis = self.vars['emojis']
+        self.icons = self.vars['icons']
+
+    @commands.command(description="Force verify someone's Riot ID (skipping verification process)")
+    @commands.has_permissions(manage_roles=True)
+    async def setup(self, ctx: commands.Context):
+        await ctx.channel.send(embed=embed.Embed(description="Thanks for using Combatant! Here's the super simple setup guide!"))
+
+        if not get(guild.roles, name="Host"):
+            ctx.guild.create_role(name="Host")
+
+        if not get(guild.roles, name="Valorant Linked"):
+            ctx.guild.create_role(name="Valorant Linked")
+
+        await ctx.channel.send(embed=embed.Embed(description="The first thing you have to do is make a channel (ideally named `#link-valorant`) where people can't talk. This channel will be for the account verification message. Once you have made it, type **next**.", footer="To cancel, type **cancel**"))
+        try:
+            confirmation = await self.bot.wait_for('message', check=check, timeout=600)
+            if(confirmation == "next"):
+                continue
+            if(confirmation == "cancel"):
+                await ctx.channel.send(embed=embed.Embed(title="Setup cancelled."))
+                return
+        except asyncio.TimeoutError:
+            await user.send(embed=embed.Embed(title="You took more than 10 minutes to tell me the channel name. Begin the setup process again with `v!setup`."))
+            return
+
+        await ctx.channel.send(embed=embed.Embed(description="Now make another channel (ideally named `#valorant-scrims`) where only people with the `Host` rank can talk. This channel will be to announce new scrims. Once you have made it, type **next**.", footer="To cancel, type **cancel**"))
+        try:
+            confirmation = await self.bot.wait_for('message', check=check, timeout=600)
+            if(confirmation == "next"):
+                continue
+            if(confirmation == "cancel"):
+                await ctx.channel.send(embed=embed.Embed(title="Setup cancelled."))
+                return
+        except asyncio.TimeoutError:
+            await user.send(embed=embed.Embed(title="You took more than 10 minutes to tell me the channel name. Begin the setup process again with `v!setup`."))
+            return
+
+        await ctx.channel.send(embed=embed.Embed(description="The last thing you have to do is create a verification message in scrim announcements channel by typing `v!createverify` in that channel. Once you have made it, type **next**.", footer="To cancel, type **cancel**"))
+        try:
+            confirmation = await self.bot.wait_for('message', check=check, timeout=600)
+            if(confirmation == "next"):
+                continue
+            if(confirmation == "cancel"):
+                await ctx.channel.send(embed=embed.Embed(title="Setup cancelled."))
+                return
+        except asyncio.TimeoutError:
+            await user.send(embed=embed.Embed(title="You took more than 10 minutes to tell me the channel name. Begin the setup process again with `v!setup`."))
+            return
+
+        await ctx.channel.send(embed=embed.Embed(description="You're done! To start matches, type `v!startmatch <description` in the scrim announcements channel!"))
--- utils/embed.py
+++ utils/embed.py
@@ -1,4 +1,5 @@
 import discord
 
-def Embed(description=None, image=None, title=None, color=0xe82c3f):
-    return discord.Embed(description=description, image=image, title=title, color=color)
+
+def Embed(description=None, image=None, title=None, color=0xe82c3f, footer=None):
+    return discord.Embed(description=description, image=image, title=title, color=color).set_footer(text=footer)
