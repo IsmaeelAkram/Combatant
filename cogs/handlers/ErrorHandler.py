d5adf00e4a52bccae715c4ffbc00671ac47f3300
ismaeelakram <mathcenter562@gmail.com>
2020-06-21 22:26:34 -0400

    Update ErrorHandler.py

--- cogs/handlers/ErrorHandler.py
+++ cogs/handlers/ErrorHandler.py
@@ -11,10 +11,9 @@ class ErrorHandler(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         if('You' in str(error)):
-            softErrorEmbed = embed.Embed(
-                title="Oops!", description=str(error))
+            softErrorEmbed = embed.Embed(title="Oops!", description=str(error))
             await ctx.channel.send(embed=embed.Embed())
         else:
-            hardErrorEmbed = embed.Embed(
-                title="Oops! An error seems to have occurred.", description=str(error), footer="Please report this to Mahjestic#9700.")
+            hardErrorEmbed = embed.Embed(title="Oops! An error seems to have occurred.", description=str(
+                error), footer="Please report this to Mahjestic#9700.")
             await ctx.channel.send(embed=hardErrorEmbed)
