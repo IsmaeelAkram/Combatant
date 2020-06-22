42eed86d1428fe580c0856d578db6c06c8f28f4e
ismaeelakram <mathcenter562@gmail.com>
2020-06-21 22:04:51 -0400

    Update embed.py

--- utils/embed.py
+++ utils/embed.py
@@ -2,4 +2,8 @@ import discord
 
 
 def Embed(description=None, image=None, title=None, color=0xe82c3f, footer=None):
-    return discord.Embed(description=description, image=image, title=title, color=color).set_footer(text=footer)
+    embed = discord.Embed(description=description,
+                          image=image, title=title, color=color)
+    if footer != None:
+        embed.set_footer(text=footer)
+    return embed
